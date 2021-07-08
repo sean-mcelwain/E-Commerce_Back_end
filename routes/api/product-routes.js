f80a0ca6770bfaf1e6aa0d6c893088a65bd4c341
sean-mcelwain <79949547+sean-mcelwain@users.noreply.github.com>
2021-07-08 16:35:41 -0500

    updated routes, structure found in product-routes example was my model for the rest that the comments indicated

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,12 +7,53 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  Product.findAll(
+    {
+      include: [
+      {
+        model: Category,
+        attributes: ["category_name"],
+      },
+      {
+        model: Tag,
+        attributes: ["tag_name"],
+      },
+    ],
+    }
+  )
+  .then((productData) => res.json(productData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    })
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  Product.findOne(
+    {
+      where: {
+        id: req.params.id,
+      },
+      include: [
+        {
+          model: Category,
+          attributes: ["category_name"],
+        },
+        {
+          model: Tag,
+          attributes: ["tag_name"],
+        },
+      ],
+    }
+  )
+  .then((productData) => res.json(productData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    })
 });
 
 // create new product
@@ -57,7 +98,13 @@ router.put('/:id', (req, res) => {
   })
     .then((product) => {
       // find all associated tags from ProductTag
-      return ProductTag.findAll({ where: { product_id: req.params.id } });
+      return ProductTag.findAll(
+        { 
+          where: { 
+            product_id: req.params.id 
+          } 
+        }
+      );
     })
     .then((productTags) => {
       // get list of current tag_ids
@@ -91,6 +138,24 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy(
+    {
+      where: {
+        id: req.params.id,
+      },
+    }
+  )
+  .then((productData) => {
+    if (!productData) {
+      res.status(404).json({message: "No Product found with the the ID provided"});
+      return;
+    }
+    res.json(productData);
+  })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
